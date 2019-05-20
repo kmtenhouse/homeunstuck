f2bbc953311c6e84477fd5f6b9ab9361ff62fd9c
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-05-19 17:06:51 -0700

    Merging from master

---
+++ background.js
@@ -0,0 +1,5 @@
+chrome.runtime.onInstalled.addListener(function () {
+    //runs when the extension is first installed only
+    
+    //(TO-DO) initialize the extension with our default settings
+});
