1059c5e8bbcb785d812b3697c35192e8d1b727a9
Tassa Tenhouse <kmtenhouse@gmail.com>
2019-05-20 23:14:09 -0700

    Refactored the quirk mapper to be promise-based

--- content.js
+++ content.js
@@ -1,20 +1,21 @@
 //GLOBAL VARIABLES
 var vastErrorQuirks = null;
-initializeQuirkList(); //to-do: refactor 
 
 //MAIN SCRIPT
 //Attach a mutation listener to the entire document
 var observer = new MutationObserver(function (mutations) {
-    if (vastErrorQuirks === null) { 
-        initializeQuirkList(); //to-do: refactor so this continues with execution after setting
-    } else {
-        mutations.forEach(function (mutation) {
-            if (mutation.target.nodeName !== 'SCRIPT') {
-                //first, make sure the quirk is defined
-                traverseDOM(mutation.target);
-            }
+    getQuirkMap()
+        .then(function (quirkMap) {
+            vastErrorQuirks = quirkMap;
+            mutations.forEach(function (mutation) {
+                if (mutation.target.nodeName !== 'SCRIPT') {
+                    traverseDOM(mutation.target);
+                }
+            });
+        })
+        .catch(function (err) {
+            console.log("An error has occurred reading the quirk map!");
         });
-    }
 });
 
 var observerConfig = {
@@ -52,52 +53,25 @@ function traverseDOM(targetNode) {
 }
 
 //INITIALIZE VARIABLES
-function initializeQuirkList() {
-    var quirkMap = new Map([
-        ["murrit", murrit],
-        ["laivan", laivan],
-        ["arcjec", arcjec],
-        ["tazsia", tazsia],
-        ["albion", albion],
-        ["ellsee", ellsee],
-        ["occeus", occeus],
-        ["dismas", dismas],
-        ["sovara", sovara],
-        ["snakepeople", arcjecDenizens],
-        ["guardianspirit", albionGuardian],
-        ["hamifi", hamifi],
-        ["sestro", sestro],
-        ["rodere", rodere],
-        ["vellia", vellia]
-    ]);
-
-    getDataFromStorage("vastErrorSettings")
-        .then(function (allCharacters) {
-            var resultingMap = new Map();
-            for (character of allCharacters) {
-                //(TO-DO) grab the current quirk from the character instead
-                //(Right now) grab the current quirk for this character from our ur-map
-                var characterQuirk = quirkMap.get(character.name);
-                if (characterQuirk && character.enabled === true) {
-                    for (alias of character.aliases) {
-                        resultingMap.set(alias, characterQuirk);
+//Promise-based function that initializes a quirk map based on which quirks the user has enabled from the options page
+function getQuirkMap() {
+    return new Promise(function (resolve, reject) {
+        chrome.storage.sync.get('vastErrorSettings', function (savedObj) {
+            if (savedObj['vastErrorSettings'] !== null && savedObj['vastErrorSettings'] !== undefined) {
+                var baseQuirkMap = vastErrorDefaultQuirks();
+                var resultingMap = new Map();
+                for (character of allCharacters) {
+                    //(TO-DO) grab the current quirk from storage instead
+                    //(Right now) grab the current quirk from our uber-map
+                    var characterQuirk = baseQuirkMap.get(character.name);
+                    if (characterQuirk && character.enabled === true) {
+                        for (alias of character.aliases) {
+                            resultingMap.set(alias, characterQuirk);
+                        }
                     }
                 }
-            }
-            vastErrorQuirks = resultingMap;
-        })
-        .catch(function (err) {
-            console.log(err.message);
-            //TO-DO: figure out what to do here
-        });
+                resolve(resultingMap);
 
-}
-
-function getDataFromStorage(keyName) {
-    return new Promise(function (resolve, reject) {
-        chrome.storage.sync.get([keyName], function (savedObj) {
-            if (savedObj[keyName] !== null && savedObj[keyName] !== undefined) {
-                resolve(savedObj[keyName]);
             }
             else {
                 reject(new Error("No data available"));
--- vastError.js
+++ vastError.js
@@ -1,297 +1,323 @@
 //VAST ERROR QUIRK SETTINGS
 //INDIVIDUAL TROLLS 
+function vastErrorDefaultQuirks() {
+    var murrit = {
+        name: "Murrit",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: false,
+            addMissingPeriods: false
+        },
+        prefix: /^\>\(\[/,
+        suffix: /\]$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\#/g, replaceWith: 'h', isCaseSensitive: true }
+        ],
+        whiteList: []
+    };
 
-var murrit = {
-    name: "Murrit",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: false,
-        addMissingPeriods: false
-    },
-    prefix: /^\>\(\[/,
-    suffix: /\]$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\#/g, replaceWith: 'h', isCaseSensitive: true }
-    ],
-    whiteList: []
-};
+    var laivan = {
+        name: "Laivan",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: true,
+            addMissingPeriods: true
+        },
+        prefix: null,
+        suffix: /\-$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\-(?=[\!\?\,\;\.\!])/g, replaceWith: '', isCaseSensitive: false },
+            { original: /\-[\s]{1,}/g, replaceWith: '. ', isCaseSensitive: false }
+        ],
+        whiteList: []
+    };
 
-var laivan = {
-    name: "Laivan",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: true,
-        addMissingPeriods: true
-    },
-    prefix: null,
-    suffix: /\-$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\-(?=[\!\?\,\;\.\!])/g, replaceWith: '', isCaseSensitive: false },
-        { original: /\-[\s]{1,}/g, replaceWith: '. ', isCaseSensitive: false }
-    ],
-    whiteList: []
-};
+    var arcjec = {
+        name: "Arcjec",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: true,
+            addMissingPeriods: true
+        },
+        prefix: /^XDXD/,
+        suffix: /XDXD$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [],
+        whiteList: []
+    };
 
-var arcjec = {
-    name: "Arcjec",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: true,
-        addMissingPeriods: true
-    },
-    prefix: /^XDXD/,
-    suffix: /XDXD$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [],
-    whiteList: []
-};
+    var tazsia = {
+        name: "Tazsia",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: false,
+            addMissingPeriods: true
+        },
+        prefix: /^\~/,
+        suffix: /\~$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\+/g, replaceWith: 't', isCaseSensitive: true }
+        ],
+        whiteList: ['+m+']
+    };
 
-var tazsia = {
-    name: "Tazsia",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: false,
-        addMissingPeriods: true
-    },
-    prefix: /^\~/,
-    suffix: /\~$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\+/g, replaceWith: 't', isCaseSensitive: true }
-    ],
-    whiteList: ['+m+']
-};
+    var albion = {
+        name: "Albion",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: false,
+            addMissingPeriods: false
+        },
+        prefix: /^\*/,
+        suffix: /\*$/,
+        separator: {
+            replace: true,
+            original: '*',
+            replaceWith: ' '
+        },
+        substitions: [
+        ],
+        whiteList: []
+    };
 
-var albion = {
-    name: "Albion",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: false,
-        addMissingPeriods: false
-    },
-    prefix: /^\*/,
-    suffix: /\*$/,
-    separator: {
-        replace: true,
-        original: '*',
-        replaceWith: ' '
-    },
-    substitions: [
-    ],
-    whiteList: []
-};
+    var ellsee = {
+        name: "Ellsee",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: true,
+            addMissingPeriods: true
+        },
+        prefix: null,
+        suffix: null,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\Σ/g, replaceWith: 'e', isCaseSensitive: true },
+            { original: /\¡/g, replaceWith: '!', isCaseSensitive: false },
+            { original: /\¿/g, replaceWith: '?', isCaseSensitive: false }
+        ],
+        whiteList: []
+    };
 
-var ellsee = {
-    name: "Ellsee",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: true,
-        addMissingPeriods: true
-    },
-    prefix: null,
-    suffix: null,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\Σ/g, replaceWith: 'e', isCaseSensitive: true },
-        { original: /\¡/g, replaceWith: '!', isCaseSensitive: false },
-        { original: /\¿/g, replaceWith: '?', isCaseSensitive: false }
-    ],
-    whiteList: []
-};
+    var occeus = {
+        name: "Occeus",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: false,
+            addMissingPeriods: true
+        },
+        prefix: null,
+        suffix: null,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\.o\./g, replaceWith: 'o', isCaseSensitive: true },
+            { original: /\.oo\./g, replaceWith: 'oo', isCaseSensitive: true },
+            { original: /(eye|Eye)/g, replaceWith: 'I', isCaseSensitive: false }
+        ],
+        whiteList: []
+    };
 
-var occeus = {
-    name: "Occeus",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: false,
-        addMissingPeriods: true
-    },
-    prefix: null,
-    suffix: null,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\.o\./g, replaceWith: 'o', isCaseSensitive: true },
-        { original: /\.oo\./g, replaceWith: 'oo', isCaseSensitive: true },
-        { original: /(eye|Eye)/g, replaceWith: 'I', isCaseSensitive: false }
-    ],
-    whiteList: []
-};
+    var sovara = {
+        name: "Sovara",
+        sentences: {
+            enforceCase: 'propercase',
+            capitalizeSentences: false,
+            addMissingPeriods: false
+        },
+        prefix: /^\(/,
+        suffix: /\)$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [],
+        whiteList: []
+    };
 
-var sovara = {
-    name: "Sovara",
-    sentences: {
-        enforceCase: 'propercase',
-        capitalizeSentences: false,
-        addMissingPeriods: false
-    },
-    prefix: /^\(/,
-    suffix: /\)$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [],
-    whiteList: []
-};
+    var dismas = {
+        name: "Dismas",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: true,
+            addMissingPeriods: true
+        },
+        prefix: null,
+        suffix: /\/{3}$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\\\//g, replaceWith: 'v', isCaseSensitive: true },
+            { original: /\/\\/g, replaceWith: 'a', isCaseSensitive: true }
+        ],
+        whiteList: []
+    };
 
-var dismas = {
-    name: "Dismas",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: true,
-        addMissingPeriods: true
-    },
-    prefix: null,
-    suffix: /\/{3}$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\\\//g, replaceWith: 'v', isCaseSensitive: true },
-        { original: /\/\\/g, replaceWith: 'a', isCaseSensitive: true }
-    ],
-    whiteList: []
-};
+    var arcjecDenizens = {
+        name: "snakeDenizens",
+        sentences: {
+            enforceCase: 'lowercase',
+            capitalizeSentences: true,
+            addMissingPeriods: true
+        },
+        prefix: null,
+        suffix: /\/{3}$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\\\//g, replaceWith: 'v', isCaseSensitive: true },
+            { original: /\/\\/g, replaceWith: 'a', isCaseSensitive: true }
+        ],
+        whiteList: []
+    };
 
-var arcjecDenizens = {
-    name: "snakeDenizens",
-    sentences: {
-        enforceCase: 'lowercase',
-        capitalizeSentences: true,
-        addMissingPeriods: true
-    },
-    prefix: null,
-    suffix: /\/{3}$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\\\//g, replaceWith: 'v', isCaseSensitive: true },
-        { original: /\/\\/g, replaceWith: 'a', isCaseSensitive: true }
-    ],
-    whiteList: []
-};
+    var albionGuardian = {
+        name: "Guardianspirit",
+        sentences: {
+            enforceCase: 'lowercase',
+            capitalizeSentences: false,
+            addMissingPeriods: false
+        },
+        prefix: /^\*{6}/,
+        suffix: /\*{5}\-$/,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [],
+        whiteList: []
+    };
 
-var albionGuardian = {
-    name: "Guardianspirit",
-    sentences: {
-        enforceCase: 'lowercase',
-        capitalizeSentences: false,
-        addMissingPeriods: false
-    },
-    prefix: /^\*{6}/,
-    suffix: /\*{5}\-$/,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [],
-    whiteList: []
-};
+    var hamifi = {
+        name: "Hamifi",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: false,
+            addMissingPeriods: false
+        },
+        prefix: null,
+        suffix: '∞',
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [],
+        whiteList: []
+    }
 
-var hamifi = {
-    name: "Hamifi",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: false,
-        addMissingPeriods: false
-    },
-    prefix: null,
-    suffix: '∞',
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [],
-    whiteList: []
+    var sestro = {
+        name: "Sestro",
+        sentences: {
+            enforceCase: 'propercase',
+            capitalizeSentences: true,
+            addMissingPeriods: false
+        },
+        prefix: '∞',
+        suffix: null,
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [],
+        whiteList: []
+    }
+
+    var rodere = {
+        name: "Rodere",
+        sentences: {
+            enforceCase: 'propercase',
+            capitalizeSentences: true,
+            addMissingPeriods: true
+        },
+        prefix: '-',
+        suffix: '--x',
+        separator: {
+            replace: true,
+            original: '-',
+            replaceWith: ' '
+        },
+        substitions: [],
+        whiteList: []
+    }
+
+    var vellia = {
+        name: "Vellia",
+        sentences: {
+            enforceCase: null,
+            capitalizeSentences: false,
+            addMissingPeriods: false
+        },
+        prefix: '=',
+        suffix: '=',
+        separator: {
+            replace: false,
+            original: ' ',
+            replaceWith: null
+        },
+        substitions: [
+            { original: /\:t\:/g, replaceWith: 't', isCaseSensitive: false }
+        ],
+        whiteList: []
+    }
+
+    //create a map to all these trolls...
+    var quirkMap = new Map([
+        ["murrit", murrit],
+        ["laivan", laivan],
+        ["arcjec", arcjec],
+        ["tazsia", tazsia],
+        ["albion", albion],
+        ["ellsee", ellsee],
+        ["occeus", occeus],
+        ["dismas", dismas],
+        ["sovara", sovara],
+        ["snakepeople", arcjecDenizens],
+        ["guardianspirit", albionGuardian],
+        ["hamifi", hamifi],
+        ["sestro", sestro],
+        ["rodere", rodere],
+        ["vellia", vellia]
+    ]);
+
+    //...and export
+    return quirkMap;
 }
 
-var sestro = {
-    name: "Sestro",
-    sentences: {
-        enforceCase: 'propercase',
-        capitalizeSentences: true,
-        addMissingPeriods: false
-    },
-    prefix: '∞',
-    suffix: null,
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [],
-    whiteList: []
-}
 
-var rodere = {
-    name: "Rodere",
-    sentences: {
-        enforceCase: 'propercase',
-        capitalizeSentences: true,
-        addMissingPeriods: true
-    },
-    prefix: '-',
-    suffix: '--x',
-    separator: {
-        replace: true,
-        original: '-',
-        replaceWith: ' '
-    },
-    substitions: [],
-    whiteList: []
-}
 
-var vellia = {
-    name: "Vellia",
-    sentences: {
-        enforceCase: null,
-        capitalizeSentences: false,
-        addMissingPeriods: false
-    },
-    prefix: '=',
-    suffix: '=',
-    separator: {
-        replace: false,
-        original: ' ',
-        replaceWith: null
-    },
-    substitions: [
-        { original: /\:t\:/g, replaceWith: 't', isCaseSensitive: false }
-    ],
-    whiteList: []
-}
 
